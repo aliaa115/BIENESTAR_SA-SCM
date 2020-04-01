74a59f557bbbe6914c10be5eba4d276e3fe4cdee
Ligia Abril <labrild@miumg.edu.gt>
2020-03-31 22:45:49 -0600

    actualizacion de vistas de movimientos de inventarios

---
+++ SCM/SCM/CapaControladorSCM/conexionSCM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Odbc;
+
+namespace CapaControladorSCM
+{
+    public class conexionSCM
+    {
+        public OdbcConnection conexion(string bd)
+        {
+            OdbcConnection conn = new OdbcConnection("Dsn="+bd+"");// creacion de la conexion via ODBC
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+            return conn;
+        }
+
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+
+        }
+    }
+}
